99c1dc5d8c97ddbab0f46f25fe4f826d705aee87
cspeloso <chrispeloso2@gmail.com>
2025-01-17 16:06:52 -0500

    changing listening origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,6 +33,6 @@ const path = require('path');
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
